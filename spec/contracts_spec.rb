03415914268c76bdbd14c74f0a6640e0d3554185
Max White <mushishi78@gmail.com>
2015-05-10 22:33:40 +0100

    Conflicting fixture names

--- spec/contracts_spec.rb
+++ spec/contracts_spec.rb
@@ -2,7 +2,7 @@ require 'attire/initializer'
 require 'contracts'
 
 describe 'Initializer' do
-  class Foo
+  class MyClass
     include Contracts
     extend Attire::Initializer
 
@@ -12,11 +12,11 @@ describe 'Initializer' do
   end
 
   it 'should use file location from original callback' do
-    a = Foo.new(foo: 34, bar: 'yo')
+    a = MyClass.new(foo: 34, bar: 'yo')
     expect { a.send(:foo) }.to_not raise_error
   end
 
   it 'should still have contract apply to initialize' do
-    expect { Foo.new(foo: 'yo') }.to raise_error
+    expect { MyClass.new(foo: 'yo') }.to raise_error
   end
 end
